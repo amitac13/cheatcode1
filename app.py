ac9237922b11a0d0150c0970e107745141af2f6b
amitac13 <amitcogan@gmail.comm>
2024-12-24 21:04:10 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+# pass 
+# print ("hello")
+# pass
+# def helo():(pass,class) 
+# helo()
+# ar =
+# ar =[2,4,5,7]
+# print(ar)
+
+#  if 3 > 7 :
+#     print("yes")
+# else:
+#     print("no")
+
+# def find_larger_number(num1, num2):
+#     if num1 > num2:
+#         return num1
+#     else:
+#         return num2
+
+    #     def find_lower_number(num1, num2):
+    # if num1 > num2:
+    #     return num1
+    # else:
+    #     return num2
+    # def bigger(n1,n2):
+    #     if n1 > n2:
+    #         return n1
+    #      return n2
+         
+    # print(f"the bigger number is")     
+
+
+
+def user_info():
+    # Get name and email from the user
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    
+    # Print the information
+    print(f"Your name is {name} and your email is {email}")
+
+# Call the function
+user_info()
---
+++ app2.py
@@ -0,0 +1,10 @@
+def user_info():
+    # קלט מהמשתמש
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    
+    # הדפסת המידע
+    print(f"Your name is {name} and your email is {email}")
+
+# קריאה לפונקציה
+user_info()
